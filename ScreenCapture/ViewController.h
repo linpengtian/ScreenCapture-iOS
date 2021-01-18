ac7d13b08ccea5eff2d0eca579880a4072e1d74f
linpengtian <linpengtian0704@gmail.com>
2021-01-18 08:09:47 -0300

    added capture feature.

--- ScreenCapture/ViewController.h
+++ ScreenCapture/ViewController.h
@@ -8,7 +8,9 @@
 #import <UIKit/UIKit.h>
 
 @interface ViewController : UIViewController
+@property (weak, nonatomic) IBOutlet UIImageView *imageView;
 
+- (IBAction)onStart:(id)sender;
 
 @end
 
--- ScreenCapture/ViewController.m
+++ ScreenCapture/ViewController.m
@@ -19,4 +19,22 @@
 }
 
 
+- (IBAction)onStart:(id)sender {
+    
+    CGSize screenSize = [[UIScreen mainScreen] applicationFrame].size;
+    CGColorSpaceRef colorSpaceRef = CGColorSpaceCreateDeviceRGB();
+    CGContextRef ctx = CGBitmapContextCreate(nil, screenSize.width, screenSize.height, 8, 4*(int)screenSize.width, colorSpaceRef, kCGImageAlphaPremultipliedLast);
+    CGContextTranslateCTM(ctx, 0.0, screenSize.height);
+    CGContextScaleCTM(ctx, 1.0, -1.0);
+
+    [(CALayer*)self.view.layer renderInContext:ctx];
+
+    CGImageRef cgImage = CGBitmapContextCreateImage(ctx);
+    UIImage *image = [UIImage imageWithCGImage:cgImage];
+    self.imageView.image = image;
+    CGImageRelease(cgImage);
+    CGContextRelease(ctx);
+    [UIImageJPEGRepresentation(image, 1.0) writeToFile:@"screen.jpg" atomically:NO];
+    
+}
 @end
